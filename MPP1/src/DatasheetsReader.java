214a0027697edb380ec5025bde056efd0a8114cf
micha <m@a.com>
2024-03-08 18:18:38 +0100

    added file reader

---
+++ MPP1/src/DatasheetsReader.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatasheetsReader {
+
+    BufferedReader reader;
+    DatasheetsReader(String path) throws FileNotFoundException {
+        this.reader = new BufferedReader(new FileReader(path));
+    }
+
+    List<String[]> getData() {
+        try {
+            List<String[]> data = new ArrayList<>();
+
+            while (this.reader.ready()) {
+                String s = this.reader.readLine();
+                String[] temp = s.split("[ \\t]{2,}"); // cut
+
+                for (int i=0; i < temp.length; i++)
+                    temp[i] = temp[i].replaceAll(" ", ""); // delate space
+
+                data.add(temp);
+            }
+
+            return data;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
--- MPP1/src/Main.java
+++ MPP1/src/Main.java
@@ -1,5 +1,22 @@
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            DatasheetsReader reader = new DatasheetsReader("datasheets/iris_training.txt");
+            List<String[]> data = reader.getData();
+
+            String test[] = data.get(0);
+            for (String x : test)
+                System.out.println(x);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+
     }
 }
